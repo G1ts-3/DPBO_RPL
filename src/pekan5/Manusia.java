8a8fedf773cb5b7a7bde2ae19b57b28afd8bc2f9
Git <raghidmuhammad3@gmail.com>
2026-03-31 14:45:16 +0700

    tambah class manusia

---
+++ src/pekan5/Manusia.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pekan5;
+
+/**
+ *
+ * @author hi
+ */
+public class Manusia {
+    private String nama;
+    private int umur;
+
+    public void setNama(String nama){
+        this.nama=nama;
+    }
+    public String getNama(){
+        return nama;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+    
+    
+}
