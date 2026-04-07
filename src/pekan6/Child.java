ed33dca44f99dfc8e811d7e3dfdd47755158ef58
Git <raghidmuhammad3@gmail.com>
2026-04-07 17:32:58 +0700

    week 6

---
+++ src/pekan6/Child.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pekan6;
+
+/**
+ *
+ * @author hi
+ */
+public class Child extends Parent{
+      // Ctrl + Shift + C
+//    public void doAfterEat(){
+//        System.out.println("Saya sikat gigi");
+//    }
+}
---
+++ src/pekan6/Main2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pekan6;
+
+/**
+ *
+ * @author hi
+ */
+public class Main2 {
+    // Polymorphism
+    // Overloading --> Statis (terjadi di class yg sama)
+    // Overriding --> Dinamis (terjadi di class berbeda)
+    public static void main(String[] args) {
+        Child c = new Child();
+        
+        c.doAfterEat();
+    }
+}
